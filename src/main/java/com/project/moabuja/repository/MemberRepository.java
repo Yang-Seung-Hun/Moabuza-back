320dc9c60e056b806b96651f18c1f9be666c8c71
park minwoo <90lastday@gmail.com>
2022-03-13 03:23:20 +0900

    [ADD] memberRepository 로그인 위한 쿼리 추가

--- src/main/java/com/project/moabuja/repository/MemberRepository.java
+++ src/main/java/com/project/moabuja/repository/MemberRepository.java
@@ -2,10 +2,17 @@ package com.project.moabuja.repository;
 
 import com.project.moabuja.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findMemberByNickname(String nickname);
+
+    @Query("select m from Member m where m.email  = :email")
+    Member findByEmail(@Param("email") String email);
+
+    boolean existsByEmail(@Param("email") String email);
 }
