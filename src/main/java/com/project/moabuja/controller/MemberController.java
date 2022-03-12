ac790328961ed39890cfbbbf42317a8b28cb2eb2
park minwoo <90lastday@gmail.com>
2022-03-13 03:29:16 +0900

    [ADD] member controller 추가

---
+++ src/main/java/com/project/moabuja/controller/MemberController.java
@@ -0,0 +1,59 @@
+package com.project.moabuja.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.project.moabuja.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 카카오 로그인 api
+    @GetMapping("/user/kakao/callback")
+    public ResponseEntity kakaoLogin(@RequestParam String code) throws JsonProcessingException {
+        TokenDto dto = memberService.kakaoLogin(code);
+        CustomResponseEntity response = CustomResponseEntity.builder()
+                .authorization(null)
+                .code(HttpStatus.OK)
+                .message("어세스토큰 : authorization")
+                .data(dto)  // data 안에 access, refresh  두개 다 담겨있다.
+                .build();
+        return response.responseAll();
+    }
+
+    // 닉네임, 캐릭터 선택 api
+    @PutMapping("/member/info")
+    public ResponseEntity update(@RequestBody MemberUpdateRequestDto dto,
+                                 @AuthenticationPrincipal UserDetailsImpl userDetails) throws JsonProcessingException {
+        String email = userDetails.getUsername();
+        // 회원이 캐릭터랑 닉네임 설정한 경우
+        return memberService.updateMemberInfo(dto, email);
+    }
+
+    // access 토큰 만료 시 재발급 api : access, refresh 모두 재발급
+    @GetMapping("/api/reissue")
+    public ResponseEntity reissue(HttpServletRequest request){
+        System.out.println("-------------컨트롤러 reissue");
+        TokenDto tokenDto = memberService.reissue(request);
+        CustomResponseEntity response = CustomResponseEntity.builder()
+                .data(tokenDto)
+                .message("Redis 저장 성공")
+                .code(HttpStatus.OK)
+                .build();
+        return response.responseAll();
+    }
+
+    @GetMapping("/api/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request){
+        return memberService.logout(request);
+    }
+
+}
