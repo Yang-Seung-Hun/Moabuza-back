17c082b986ef15b36f71c148cc071aa99884406a
park minwoo <90lastday@gmail.com>
2022-03-13 03:28:24 +0900

    [ADD] security configuration 추가 [ADD] cors configuration 추가

---
+++ src/main/java/com/project/moabuja/config/WebSercurityConfig.java
@@ -0,0 +1,46 @@
+package com.project.moabuja.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSercurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtProvider;
+    private final RedisTemplate redisTemplate;
+    private final CorsFilter corsFilter;
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**"); }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class);
+        http.httpBasic().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .authorizeRequests()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers(HttpMethod.GET, "/user/kakao/callback").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/reissue").permitAll()
+                .anyRequest().authenticated();
+
+        http    .addFilterBefore(new CustomAuthenticationFilter(jwtProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class);
+    }
+}
