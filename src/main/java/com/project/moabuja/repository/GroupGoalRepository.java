99e0a2a947a8d7f26e8c59864856ee793e67b53d
Yang-Seung-Hun <simononze@naver.com>
2022-03-15 22:17:55 +0900

    GroupGoalService getGroupMemberCandidates,exitChallenge 추가

--- src/main/java/com/project/moabuja/repository/GroupGoalRepository.java
+++ src/main/java/com/project/moabuja/repository/GroupGoalRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupGoalRepository extends JpaRepository<GroupGoal,Long> {
 
-
+    public void deleteGroupGoalById(Long id);
 
 
 }
--- src/main/java/com/project/moabuja/service/GroupGoalServiceImp.java
+++ src/main/java/com/project/moabuja/service/GroupGoalServiceImp.java
@@ -1,5 +1,6 @@
 package com.project.moabuja.service;
 
+import com.project.moabuja.domain.friend.Friend;
 import com.project.moabuja.domain.goal.ChallengeGoal;
 import com.project.moabuja.domain.goal.DoneGoal;
 import com.project.moabuja.domain.goal.DoneGoalType;
@@ -9,6 +10,7 @@ import com.project.moabuja.domain.record.Record;
 import com.project.moabuja.domain.record.RecordType;
 import com.project.moabuja.dto.request.goal.CreateGroupRequestDto;
 import com.project.moabuja.dto.response.goal.*;
+import com.project.moabuja.repository.FriendRepository;
 import com.project.moabuja.repository.GroupGoalRepository;
 import com.project.moabuja.repository.MemberRepository;
 import com.project.moabuja.repository.RecordRepository;
@@ -29,8 +31,10 @@ public class GroupGoalServiceImp implements GroupGoalService{
     private final MemberRepository memberRepository;
     private final GroupGoalRepository groupGoalRepository;
     private final RecordRepository recordRepository;
+    private final FriendRepository friendRepository;
 
     @Override
+    @Transactional
     public GroupGoal save(CreateGroupRequestDto groupRequestDto, Member current) {
 
         Optional<Member> currentUserTmp = memberRepository.findById(current.getId());
@@ -104,12 +108,55 @@ public class GroupGoalServiceImp implements GroupGoalService{
 
     @Override
     public CreateGroupResponseDto getGroupMemberCandidates(Member currentUser) {
-        return null;
+
+        List<Friend> friends = friendRepository.findFriendsByMember(currentUser);
+        List<CreateGroupMemberDto> groupMembers = new ArrayList<>();
+
+        if (friends.size() == 0){
+            return new CreateGroupResponseDto(groupMembers);
+        }
+
+        for(Friend friend : friends){
+            //친구의 그룹 골을 확인
+            Optional<Member> friendById = memberRepository.findById(friend.getFriend());
+            Optional<GroupGoal> friendGroupGoal = Optional.ofNullable(friendById.get().getGroupGoal());
+
+            if(friendGroupGoal.isPresent()){
+                //이미 진행중인 챌린지 있음
+                if(friendGroupGoal.get().isAcceptedGroup()){
+                    if (friendById.isPresent()){
+                        groupMembers.add(new CreateGroupMemberDto(friendById.get().getNickname(),false));
+                    }
+                }
+                //진행중인 챌린지 없고, 대기만 있음
+                else{
+                    if(friendById.isPresent()){
+                        groupMembers.add(new CreateGroupMemberDto(friendById.get().getNickname(),true));
+                    }
+                }
+            }
+            else{//초대받은 챌린지 없고 진행중인것도 없을때
+                if (friendById.isPresent()){
+                    groupMembers.add(new CreateGroupMemberDto(friendById.get().getNickname(),true));
+                }
+            }
+        }
+
+        return new CreateGroupResponseDto(groupMembers);
     }
 
     @Override
+    @Transactional
     public void exitChallenge(Long id) {
 
-    }
+        Optional<GroupGoal> groupGoal = groupGoalRepository.findById(id);
+        if(groupGoal.isPresent() && !groupGoal.get().isAcceptedGroup()){
 
+            List<Member> members = groupGoal.get().getMembers();
+            while (members.size() > 0){
+                groupGoal.get().removeMember(members.get(0));
+            }
+            groupGoalRepository.deleteGroupGoalById(id);
+        }
+    }
 }
