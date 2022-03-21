817c98d59bee1361286a549ab9da29701121bb1b
panghoon <leegwanghoon95@gmail.com>
2022-03-21 12:37:14 +0900

    [FEAT] : add ExceptionHadler

--- src/main/java/com/project/moabuja/dto/request/member/MemberUpdateRequestDto.java
+++ src/main/java/com/project/moabuja/dto/request/member/MemberUpdateRequestDto.java
@@ -8,13 +8,21 @@ import lombok.Setter;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 @Setter
 public class MemberUpdateRequestDto {
 
+    @Size(max = 8, message = "8자 이하 입력가능")
+    @Pattern(regexp = "^[a-zA-Z0-9ㄱ-ㅎ가-힣]*$")
+    @NotNull
     private String nickname;
+
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Hero hero;
 }
