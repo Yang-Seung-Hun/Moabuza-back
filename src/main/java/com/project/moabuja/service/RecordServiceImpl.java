0e85552a896d6c5e219a6a13e888896881e3daad
Yang-Seung-Hun <simononze@naver.com>
2022-03-25 03:54:38 +0900

    [FIX] RecordServiceImpl alarm logic 추가

--- src/main/java/com/project/moabuja/service/RecordServiceImpl.java
+++ src/main/java/com/project/moabuja/service/RecordServiceImpl.java
@@ -1,5 +1,8 @@
 package com.project.moabuja.service;
 
+import com.project.moabuja.domain.alarm.Alarm;
+import com.project.moabuja.domain.alarm.AlarmDetailType;
+import com.project.moabuja.domain.alarm.AlarmType;
 import com.project.moabuja.domain.goal.DoneGoal;
 import com.project.moabuja.domain.goal.GoalType;
 import com.project.moabuja.domain.goal.GroupGoal;
@@ -11,6 +14,7 @@ import com.project.moabuja.dto.request.record.RecordRequestDto;
 import com.project.moabuja.dto.response.record.DayListResponseDto;
 import com.project.moabuja.dto.response.record.DayRecordResponseDto;
 import com.project.moabuja.dto.response.record.RecordResponseDto;
+import com.project.moabuja.repository.AlarmRepository;
 import com.project.moabuja.repository.DoneGoalRepository;
 import com.project.moabuja.repository.MemberRepository;
 import com.project.moabuja.repository.RecordRepository;
@@ -34,6 +38,7 @@ public class RecordServiceImpl implements RecordService{
     private final RecordRepository recordRepository;
     private final DoneGoalRepository doneGoalRepository;
     private final MemberRepository memberRepository;
+    private final AlarmRepository alarmRepository;
 
     @Transactional
     @Override
@@ -49,10 +54,31 @@ public class RecordServiceImpl implements RecordService{
 
         //type이 challenge일때 목표완료됐는지 확인
         if(recordRequestDto.getRecordType() == RecordType.challenge){
+
+            /**
+             * 여기 알람 보내는거 추가해주시면 됩니다!!!!!
+             */
+            List<Member> members = currentMember.getChallengeGoal().getMembers();
+            for (Member member : members) {
+                Alarm alarm = new Alarm(AlarmType.CHALLENGE, AlarmDetailType.record, currentMember.getChallengeGoal().getChallengeGoalName(),
+                        currentMember.getChallengeGoal().getChallengeGoalAmount(), null, currentMember.getNickname(), member);
+                alarmRepository.save(alarm);
+            }
+
             int goalAmount = currentMember.getChallengeGoal().getChallengeGoalAmount();
             int currentAmount = countCurrentChallenge(currentMember);
 
             if(currentAmount >= goalAmount){
+
+                /**
+                 * 여기 알람 보내는거 추가해주시면 됩니다!!!!!
+                 */
+                for (Member member : members) {
+                    Alarm alarm = new Alarm(AlarmType.CHALLENGE, AlarmDetailType.success, currentMember.getChallengeGoal().getChallengeGoalName(),
+                            currentMember.getChallengeGoal().getChallengeGoalAmount(), null, currentMember.getNickname(), member);
+                    alarmRepository.save(alarm);
+                }
+
                 //완료 로직
                 RecordRequestDto dto1 = new RecordRequestDto(RecordType.challenge, recordRequestDto.getRecordDate(), "도전해부자 완료!!", -1 * currentAmount);
                 RecordRequestDto dto2 = new RecordRequestDto(RecordType.income, recordRequestDto.getRecordDate(), "도전해부자 완료!!", currentAmount);
@@ -75,6 +101,17 @@ public class RecordServiceImpl implements RecordService{
 
         //group goal 완료 로직
         else if(recordRequestDto.getRecordType() == RecordType.group){
+
+            /**
+             * 여기 알람 보내는거 추가해주시면 됩니다!!!!!
+             */
+            List<Member> members = currentMember.getGroupGoal().getMembers();
+            for (Member member : members) {
+                Alarm alarm = new Alarm(AlarmType.GROUP, AlarmDetailType.record, currentMember.getGroupGoal().getGroupGoalName(),
+                        currentMember.getGroupGoal().getGroupGoalAmount(), null, currentMember.getNickname(), member);
+                alarmRepository.save(alarm);
+            }
+
             int goalAmount = currentMember.getGroupGoal().getGroupGoalAmount();
 
             GroupGoal groupGoal = currentMember.getGroupGoal();
@@ -86,6 +123,16 @@ public class RecordServiceImpl implements RecordService{
 
             if(currentAmount >= goalAmount){
                 //완료 로직 => 각 사용자 저금통에서 각자 낸 만큼 빼주기(도전해부자랑 다름, 도전해부자는 다시 넣어주는것 까지 있음)
+
+                /**
+                 * 여기 알람 보내는거 추가해주시면 됩니다!!!!!
+                 */
+                for (Member member : members) {
+                    Alarm alarm = new Alarm(AlarmType.GROUP, AlarmDetailType.success, currentMember.getGroupGoal().getGroupGoalName(),
+                            currentMember.getGroupGoal().getGroupGoalAmount(), null, currentMember.getNickname(), member);
+                    alarmRepository.save(alarm);
+                }
+
                 for (Member member : separateAmount.keySet()) {
                     RecordRequestDto dto = new RecordRequestDto(RecordType.group, recordRequestDto.getRecordDate(), "같이해부자 완료!!", -1*separateAmount.get(member));
                     Record minusRecord = new Record(dto, member);
