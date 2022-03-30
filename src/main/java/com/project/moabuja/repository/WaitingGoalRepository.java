ffa6359f44547ccd0d346d8856bbe411d7d1085d
park minwoo <90lastday@gmail.com>
2022-03-30 23:30:59 +0900

    [FIX] waitingGoal List

--- src/main/java/com/project/moabuja/repository/WaitingGoalRepository.java
+++ src/main/java/com/project/moabuja/repository/WaitingGoalRepository.java
@@ -1,9 +1,12 @@
 package com.project.moabuja.repository;
 
+import com.project.moabuja.domain.goal.GoalType;
+import com.project.moabuja.domain.goal.MemberWaitingGoal;
 import com.project.moabuja.domain.goal.WaitingGoal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WaitingGoalRepository extends JpaRepository<WaitingGoal,Long> {
 
+    WaitingGoal findWaitingGoalByMemberWaitingGoalAndGoalType(MemberWaitingGoal memberWaitingGoal, GoalType goalType);
     WaitingGoal findWaitingGoalById(Long waitingGoalId);
 }
