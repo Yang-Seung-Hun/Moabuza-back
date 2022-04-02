019764e6ec664c4208c5512966675c67603a7b39
panghoon <leegwanghoon95@gmail.com>
2022-04-02 17:54:13 +0900

    [FEAT] : nginx -ing / fix gradle.yml

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,7 +3,7 @@
 function find_idle_profile()
 {
     # curl 결과로 연결할 서비스 결정
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
