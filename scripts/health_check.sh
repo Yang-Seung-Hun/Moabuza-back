fd674f7a5655fa5d2812d8be1fd7047f8c434d13
panghoon <leegwanghoon95@gmail.com>
2022-04-02 13:35:21 +0900

    [FEAT] : nginx -ing

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -5,10 +5,10 @@ CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 # Toggle port Number
-if [ "${CURRENT_PORT}" -eq 8080 ]; then
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
   TARGET_PORT=8081
-elif [ "${CURRENT_PORT}" -eq 8081 ]; then
-  TARGET_PORT=8080
 else
   echo "> No WAS is connected to nginx"
   exit 1
@@ -21,7 +21,7 @@ do
   echo "> #${RETRY_COUNT} trying..."
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${TARGET_PORT}/health)
 
-  if [ "${RESPONSE_CODE}" -eq 200 ]; then
+  if [ ${RESPONSE_CODE} -eq 200 ]; then
 	  echo "> New WAS successfully running"
 	  exit 0
   elif [ ${RETRY_COUNT} -eq 10 ]; then
