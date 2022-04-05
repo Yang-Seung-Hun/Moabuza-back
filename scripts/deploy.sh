5bacffd1328794c845dff4cce49db69fc811b671
panghoon <leegwanghoon95@gmail.com>
2022-04-05 22:00:35 +0900

    [FEAT] : port change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,7 @@ echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
 
+
 echo "$CURRENT_PID"
 
 if [ -z $CURRENT_PID ]; then
@@ -22,6 +23,7 @@ fi
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
 
 echo "> JAR Name: $JAR_NAME"
 
